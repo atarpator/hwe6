d6e1704264dcdc0b6d3d1c2575ee666606ba8785
atarpator <atarpator@gmail.com>
2020-06-14 21:48:33 +0300

    hwe6

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hwe6
- 
+﻿# Калькулятор Фибоначчи
+
+Клонировать проект
+
+sudo docker-compose build
+
+sudo docker-compose up
+
+Открыть по адресу http://localhost:5000/<int:> 
---
+++ app/app.py
@@ -0,0 +1,31 @@
+﻿from redis import Redis
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
+REDIS_HOST = os.environ.get('REDIS_HOST')
+redis_client = Redis(host=REDIS_HOST)
+print(REDIS_HOST)
+
+@app.route('/')
+def hello_world():
+    return 'Fibonacci calculator'
+
+def get_fibonacci(number):
+    if (number == 0) or (number == 1): 
+        return number
+    return get_fibonacci(number-1) + get_fibonacci(number-2)
+
+@app.route("/<number>", methods=['GET'])
+def get_fibonacci_api(number):
+    number = int(number)
+    stored_value = redis_client.get(number)
+    if stored_value:
+        return jsonify({number: stored_value.decode()}), 200
+    new_value = get_fibonacci(number)
+    redis_client.set(number, new_value)
+    return jsonify({number: new_value}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=port)
